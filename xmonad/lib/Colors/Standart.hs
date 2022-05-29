a4cc1bcc07c88d0d1c1d22c54ec7333937dd0a77
Ftamino <83065176+Ftamino@users.noreply.github.com>
2022-05-29 16:11:54 +0200

    Add files via upload

---
+++ xmonad/lib/Colors/Standart.hs
@@ -0,0 +1,29 @@
+module Colors.Standart where
+
+import XMonad
+
+colorScheme = "nord"
+
+colorBack = "#ABB8C3" --Color for border of a unfocused window
+colorFore = "#166AE0" --Color for border of a focused window
+
+color01 = "#00C504" --Color for visible workspace in xmobar
+color02 = "#E41266" --Color for current workspace in xmobar
+color03 = "#15D452" --Color for the bar under current workspace in xmobar
+color04 = "#1283DE" --Color for invisible workspace in xmobar
+color05 = "#E47E12" --Color for the bar under invisible workspace with window in xmobar
+color06 = "#1283DE" --Color for invisible workspace with no windows in xmobar
+color07 = "#1ED5D5" --Color for window title in xmobar
+color08 = "#1ED5D5" --Color for window layout in xmobar
+color09 = "#455A64" --Color for Seperator in xmobar
+color10 = "#343d46"
+color11 = "#99C794"
+color12 = "#FAC863"
+color13 = "#6699cc"
+color14 = "#c594c5"
+color15 = "#5fb3b3"
+color16 = "#d8dee9"
+
+colorTrayer :: String
+colorTrayer = "--tint 0x2E3440"
+
