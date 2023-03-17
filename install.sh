629b04fda91aee3cf239348a6db7e95ddd0debfd
Ftamino <83065176+Ftamino@users.noreply.github.com>
2023-03-17 20:01:46 +0100

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,45 +1,43 @@
+#!/bin/bash
+
 echo Starting Installer
 echo Updating
 
-sudo apt update
-sudo apt upgrade -y
+#if [ -f /etc/debian_version ]; then
+#  sudo apt update
+#  sudo apt upgrade -y
+#elif [ -f /etc/arch-release ]; then
+#  sudo pacman -Syu
+#else
+#  echo "Unsupported distribution."
+#  exit 1
+#fi
 
 echo Install dependencies
 
-sudo apt-get install -y xmonad xmobar neofetch lxpolkit dunst fish rofi picom copyq nitrogen emacs kitty flatpak arandr python3 curl wget 
-sudo pacman -Sy xmonad xmobar neofetch lxpolkit dunst fish rofi picom copyq nitrogen emacs kitty flatpak arandr python3 curl wget
-flatpak install flameshot
-
-curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+#if [ -f /etc/debian_version ]; then
+#  sudo apt-get install -y xmonad xmobar neofetch lxpolkit dunst fish picom copyq nitrogen emacs kitty flatpak arandr python3 curl wget 
+#elif [ -f /etc/arch-release ]; then
+#  sudo pacman -S xmonad xmobar neofetch lxpolkit dunst fish picom copyq nitrogen emacs kitty flatpak arandr python3 curl wget
+#  flatpak install flameshot
+#else
+#  echo "Unsupported distribution."
+#  exit 1
+#fi
 
 echo Install Fish Theme
+curl -L https://get.oh-my.fish | fish
 omf install bobthefish
 
 echo Installing Doom Emacs
 rm -rf ~/.emacs.d
-git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.emacs.d
+git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
 ~/.emacs.d/bin/doom install
 
 echo Install Nerd Fonts
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/3270.zip
 unzip 3270.zip -d ~/.fonts
 fc-cache -fv
-
-echo Install rofi Themes
-
-git clone https://github.com/lr-tech/rofi-themes-collection.git
-cd rofi-themes-collection
-mkdir -p ~/.local/share/rofi/themes/
-cp themes/* ~/.local/share/rofi/themes/
-cd
-git clone --depth=1 https://github.com/adi1090x/rofi.git
-cd rofi
-chmod +x setup.sh
-./setup.sh
-cd
-
-echo Making xmonad dir
-mkdir ~/.xmonad
 
 echo Copying Configs
 
@@ -53,4 +51,35 @@ cp ./configuration.py ~/.configuration.py
 cp ./initial-setup.py ~/.initial-setup.py
 cp ./logout-manager.py ~/.logout-manager.py
 
+echo Making xmonad dir
+mkdir -p ~/.xmonad
+
 echo Installed
+
+# prompt user for rofi themes installation
+echo "Do you want to install rofi themes? (y/n)"
+read install_rofi_themes
+
+if [[ $install_rofi_themes =~ ^[Yy]$ ]]
+then
+  if [ -f /etc/debian_version ]; then
+    sudo apt-get install -y git
+  elif [ -f /etc/arch-release ]; then
+    sudo pacman -S git
+  else
+    echo "Unsupported distribution."
+    exit 1
+  fi
+
+  git clone https://github.com/lr-tech/rofi-themes-collection.git
+  cd rofi-themes-collection
+  mkdir -p ~/.local/share/rofi/themes/
+  cp themes/* ~/.local/share/rofi/themes/
+  cd
+  git clone --depth=1 https://github.com/adi1090x/rofi.git
+  cd rofi
+  chmod +x setup.sh
+  ./setup.sh
+  cd
+fi
+
